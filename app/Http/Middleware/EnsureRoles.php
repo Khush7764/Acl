7f385113b1e2e08c9ccd553d47de49c9883d8937
Bhargav Chhaya <bhargav.chhaya@tradofina.com>
2022-09-21 15:17:14 +0530

    added middleware

---
+++ app/Http/Middleware/EnsureRoles.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureRoles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $roles = [])
+    {
+        dd($roles);
+        if (! $request->user()->hasRole($role)) {
+            // Redirect...
+            return back();
+        }
+        return $next($request);
+    }
+}
