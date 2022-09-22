f0f1788dcb19684daab0f232b1743cc75e29e8e6
Bhargav Chhaya <bhargav.chhaya@tradofina.com>
2022-09-22 12:27:33 +0530

    added urls

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,17 @@ Route::group([
 ], function(){
     Route::get('all-permissions/{id}', [AclController::class, 'getUserPermissions']);
     Route::get('all-permissions', [AclController::class, 'getAllPermissions']);
+
+    Route::get('freshGrid', [PurpleController::class, 'freshGrid']);
+    Route::get('loanmatrix/freshgrid/3month', [PurpleController::class, 'threeMonthList']);
+    Route::get('loanmatrix/freshgrid/3month/edit', [PurpleController::class, 'threeMonthEdit']);
+    Route::get('loanmatrix/freshgrid/62Days', [PurpleController::class, 'sixtyTwoDaysList']);
+    Route::get('loanmatrix/freshgrid/62Days/edit', [PurpleController::class, 'sixtyTwoDaysEdit']);
+    Route::get('loanmatrix/freshgrid/6Month', [PurpleController::class, 'sixMonthList']);
+    Route::get('loanmatrix/freshgrid/6Month/edit', [PurpleController::class, 'sixMonthEdit']);
+    Route::get('loanmatrix/freshgrid/sendexcelreport', [PurpleController::class, 'sendExcelReport']);
+    Route::get('loanmatrix/freshgrid/addnewtemplate', [PurpleController::class, 'addNewTemplate']);
+    Route::get('transactionsList', [PurpleController::class, 'transactionsList']);
 });
 Route::get('all-permissions', [AclController::class, 'getAllPermissions']);
 Route::get('all-roles', [AclController::class, 'getAllRoles']);
