174c174c8e20bb5d8e269c421665b5a290531c31
Bhargav Chhaya <bhargav.chhaya@tradofina.com>
2022-09-22 12:07:44 +0530

    added middleware

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::group([
-    'middleware' => ["EnsureRoles"]
+    'middleware' => ["ensure_roles"]
 ], function(){
-    Route::get('all-permissions', [AclController::class, 'getAllPermissions']);
     Route::get('all-permissions/{id}', [AclController::class, 'getUserPermissions']);
-    Route::get('all-roles', [AclController::class, 'getAllRoles']);
+    Route::get('all-permissions', [AclController::class, 'getAllPermissions']);
 });
+Route::get('all-permissions', [AclController::class, 'getAllPermissions']);
+Route::get('all-roles', [AclController::class, 'getAllRoles']);
